e29f1addfa903aa2551d61e4ad5ca3ca9c20bf32
abikimoz <i.m.anisimov25@gmail.com>
2023-01-09 23:35:21 +0700

    add fixImport calc

--- bin/brain-gcd.js.js
+++ bin/brain-gcd.js.js
@@ -2,9 +2,8 @@
 
 import name, { userName } from '../src/cli.js';
 import logic, { message } from '../src/index.js';
-import questionThree, { questionText, answerLogic } from '../src/games/calcData.js';
 
 name();
-logic(questionText, questionThree, answerLogic);
+logic();
 const finalMessage = (message !== 1) ? `Let's try again, ${userName}!` : `Congratulations, ${userName}!`;
 console.log(finalMessage);
---
+++ src/games/gcdData.js
@@ -0,0 +1,21 @@
+import { questionEssence } from '../index.js';
+
+export const questionText = "What is the result of the expression?";
+
+function questionThree() {
+  const oneElement = Math.ceil(Math.random() * 100);
+  const twoElement = Math.ceil(Math.random() * 100);
+  const allSigns = "+-*+-*+-*+-";
+  const randomAllSigns = allSigns[Math.ceil(Math.random() * 10)];
+  const questionEssence = `${oneElement} ${randomAllSigns} ${twoElement}`;
+  return questionEssence;
+}
+
+function answerLogic() {
+  const answerTrueEval = eval(questionEssence);
+  const answerTrue = String(answerTrueEval);
+  return answerTrue;
+}
+
+export { answerLogic };
+export default questionThree;
