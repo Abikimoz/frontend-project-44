8d07a9834facdf40d27a368f6ed1af2d8671223e
abikimoz <i.m.anisimov25@gmail.com>
2023-01-11 16:52:48 +0700

    add fix check 2

--- src/cli.js
+++ src/cli.js
@@ -3,10 +3,11 @@ import readlineSync from 'readline-sync';
 export let userName = '';
 
 function name() {
+  userName = readlineSync.question('Hello ');
   console.log('brain-games!');
   console.log('Welcome to the Brain Games!');
   userName = readlineSync.question('May I have your name? ');
-  console.log(`/Hello ${userName}/`);
+  console.log(`Hello ${userName}!`);
 }
 
 export default name;
