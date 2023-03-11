1ce054a9532ec08767906c6cff72f4679926d7d9
abikimoz <i.m.anisimov25@gmail.com>
2023-03-11 14:05:58 +0300

    вырезал pushName из cli

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,9 @@
 import readlineSync from 'readline-sync';
 
-let pushName; // проблема при последующем экспорте, не определяется переменная назначенная в функции
-
-export const userName = pushName; // замена експорта изменяемой переменной
-
 function name() {
   console.log('brain-games!');
   console.log('Welcome to the Brain Games!');
-  pushName = readlineSync.question('May I have your name? ');
+  const pushName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${pushName}!`);
   return pushName;
 }
